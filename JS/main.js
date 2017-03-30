875dbc6375ca54b39ebd738f5b6bf6a8e27a05e3
kcadesign <kace85@hotmail.com>
2017-03-30 16:08:23 +0900

    modal adjustments

--- JS/main.js
+++ JS/main.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
     
     $("#exhibits-awards").toggle(function () {
         $('#about-modal-body, .exhibits-awards-list').slideToggle("slow");
-        $("#exhibits-awards").animate({"top": "150px"}, "slow");
+        $("#exhibits-awards").animate({"top": "75px"}, "slow");
     }, function () {
         $('#about-modal-body, .exhibits-awards-list').slideToggle("slow");
-        $("#exhibits-awards").animate({"top": "1130px"}, "slow");
+        $("#exhibits-awards").animate({"top": "420px"}, "slow");
     });
     
     $('#contact').on('click', function () {
